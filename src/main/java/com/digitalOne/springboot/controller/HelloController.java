4619c5a6854e18cb41813096a586121262b7ccc9
Jefferson S Coelho <jefferson.coelho@globalhitss.com.br>
2021-09-10 17:44:59 -0300

    add controller

--- src/main/java/com/digitalOne/springboot/controller/HelloController.java
+++ src/main/java/com/digitalOne/springboot/controller/HelloController.java
@@ -1,4 +1,13 @@
 package com.digitalOne.springboot.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @GetMapping("/")
+    public String helloMessage (){
+        return "Hello, Digital One!2";
+    }
 }
